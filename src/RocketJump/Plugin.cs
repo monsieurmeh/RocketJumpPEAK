ffd42e257e61ad38f7657b0eb208afd5d6bd60ec
MonsieurMeh <mensoirmeh@gmail.com>
2025-12-11 17:14:11 -0800

    initial

---
+++ src/RocketJump/Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+
+namespace RocketJump
+{
+
+    [BepInAutoPlugin]
+    public partial class Plugin : BaseUnityPlugin
+    {
+        AssetManager assetManager = null!;
+
+        private void Awake()
+        {
+            AssetManager assetManager = new AssetManager(Logger);
+            if (!assetManager.Initialize())
+            {
+                Logger.LogError("AssetManager init failure!");
+                return;
+            }
+
+            Logger.LogInfo($"RocketJump, locked and loaded...");
+        }
+    }
+}
