caaaa56eeffee0f480be1f47b69e746b176a4ac0
MonsieurMeh <mensoirmeh@gmail.com>
2025-12-13 21:28:38 -0800

    feat: missing shit (dont want another dep)

---
+++ src/RocketJump/MissingSystemStuff.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Microsoft.CodeAnalysis
+{
+    internal sealed class EmbeddedAttribute : Attribute
+    {
+    }
+}
+
+namespace System.Runtime.CompilerServices
+{
+    internal sealed class NullableAttribute : Attribute
+    {
+        public readonly byte[] NullableFlags;
+
+        public NullableAttribute(byte P_0)
+        {
+            NullableFlags = new byte[1] { P_0 };
+        }
+
+        public NullableAttribute(byte[] P_0)
+        {
+            NullableFlags = P_0;
+        }
+    }
+
+    internal sealed class NullableContextAttribute : Attribute
+    {
+        public readonly byte Flag;
+
+        public NullableContextAttribute(byte P_0)
+        {
+            Flag = P_0;
+        }
+    }
+
+    internal sealed class RefSafetyRulesAttribute : Attribute
+    {
+        public readonly int Version;
+
+        public RefSafetyRulesAttribute(int P_0)
+        {
+            Version = P_0;
+        }
+    }
+}
+
