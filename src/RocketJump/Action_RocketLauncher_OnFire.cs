eaa6ca7c2ebdb02fd337880b8b23b788cb01401b
MonsieurMeh <mensoirmeh@gmail.com>
2025-12-13 21:28:13 -0800

    feat: fire trigger

---
+++ src/RocketJump/Action_RocketLauncher_OnFire.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RocketJump
+{
+    internal class Action_RocketLauncher_OnFire : ItemAction
+    {
+        private static readonly ManualLogSource logger = BepInEx.Logging.Logger.CreateLogSource("Action_RocketLauncher_OnFire");
+        public RocketLauncher launcher;
+
+        public override void RunAction()
+        {
+            if (launcher == null)
+            {
+                logger.LogError("null launcher!");
+                return;
+            }
+            launcher.MaybeFire();
+        }
+    }
+}
