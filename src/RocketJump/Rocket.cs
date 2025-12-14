ae2ffa090e3933ac108d86502d13ef1ec5d2bc2f
MonsieurMeh <mensoirmeh@gmail.com>
2025-12-13 21:28:21 -0800

    feat: rocket script stub

---
+++ src/RocketJump/Rocket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace RocketJump
+{
+    public class Rocket : MonoBehaviour
+    {
+        public float blastRadius = 5f;
+        public float fallTime = 0.5f;
+        public float knockback = 25f;
+        public float minFactor = 0.2f;
+        public float factorPow = 1f;
+        public float itemKnockbackModifier = 1f;
+    }
+}
